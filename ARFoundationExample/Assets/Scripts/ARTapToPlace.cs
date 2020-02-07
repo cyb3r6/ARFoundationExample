2106a2f9384ac28749ba3e877d5fc99605cc44b9
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2020-02-07 12:28:14 -0700

    Added Unity Project in LWRP

---
+++ ARFoundationExample/Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+namespace Corgan
+{
+    public class ARTapToPlace : MonoBehaviour
+    {
+        public GameObject prefab;
+        public Button modelAButton;
+        public Button modelBButton;
+        private GameObject spawnedObject;       // to hold a reference to the placed object
+
+        static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        private ARRaycastManager raycastManager;
+        
+        void Awake()
+        {
+            raycastManager = GetComponent<ARRaycastManager>();
+        }
+
+        
+        void Update()
+        {
+            // need to know if we touched the screen and where
+            if(Input.touchCount > 0)
+            {
+                Vector3 touchPosition = Input.GetTouch(0).position;         // (x,y)
+                if(raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPose = hits[0].pose;
+
+                    spawnedObject = Instantiate(prefab, hitPose.position, hitPose.rotation);
+
+                    // need to assign button onClick functions
+                    PlacedPrefab placedPrefab = spawnedObject.GetComponent<PlacedPrefab>();
+                    placedPrefab.AddToModelAButton(modelAButton);
+                    placedPrefab.AddToModelBButton(modelBButton);
+                }
+            }
+        }
+    }
+}
+
---
+++ ARFoundationExample/Assets/Scripts/PlacedPrefab.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Corgan
+{
+    public class PlacedPrefab : MonoBehaviour
+    {
+        public List<GameObject> models = new List<GameObject>();
+
+        public void AddToModelAButton(Button modelAButton)
+        {
+            modelAButton.onClick.AddListener(() => ModelA());
+        }
+        public void AddToModelBButton(Button modelBButton)
+        {
+            modelBButton.onClick.AddListener(() => ModelB());
+        }
+
+        public void ModelA()
+        {
+            foreach (GameObject gameObject in models)
+            {
+                gameObject.SetActive(false);
+            }
+            models[0].SetActive(true);
+        }
+        public void ModelB()
+        {
+            foreach (GameObject gameObject in models)
+            {
+                gameObject.SetActive(false);
+            }
+            models[1].SetActive(true);
+        }
+    }
+}
